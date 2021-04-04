0d109f33a2acbabd6142ac2dd96cfce299bc88ce
moyi <78648778+moyi-qwq@users.noreply.github.com>
2021-04-05 07:31:04 +0800

    Update scriptwjx_vj.js

--- scriptwjx_vj.js
+++ scriptwjx_vj.js
@@ -1,11 +1,11 @@
 javascript: void ((function () 
 {
-    var name1 = "董一如";
-    var id1 = "20111119";
-    var qq1 = "3358506770";
-    var xueyuan1 = "理学院";
+    var name1 = "213";
+    var id1 = "20201129";
+    var qq1 = "3358506723170";
+    var xueyuan1 = "理";
     var zhuanye1 = "信息与计算科学";
-    var phonenum1 = "134111111";
+    var phonenum1 = "13132156119";
 
     var hash = {};
     var a = document.evaluate('//input[(@type="radio") and not(@value="0")]//@name', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -54,25 +54,29 @@ javascript: void ((function ()
             preName = name;
         }
     }
-    
-    var temobj = document.getElementById("divTitle1").innerHTML;
+
 
     var objs = document.getElementsByTagName("textarea");
     
     for (var i = 0; i < objs.length; i++) {
-        objs[i].focus();
         var text = objs[i].getAttribute("name");
         var divT = "divTitle";
-        var findit = divT.concat(text[1])
-        var input = document.getElementById(findit).innerHTML;
-        var output = "";
-        if(input.indexOf("姓名") != -1) output = name1;
-        if(input.indexOf("学号") != -1) output = id1;
-        if(input.indexOf("QQ") != -1 || input.indexOf("qq") != -1) output = qq1;
-        if(input.indexOf("学院") != -1) output = xueyuan1;
-        if(input.indexOf("专业") != -1) output = zhuanye1;
-        if(input.indexOf("手机") != -1 || input.indexOf("电话") != -1 ||input.indexOf("号码") != -1) output = phonenum1;
-
+        var output = "1";
+        var regPos = / ^\d+$/;
+        if(eval(text[1]) > 0)
+        {
+            var findit = divT.concat(text[1]);
+            var context1 = document.getElementById(findit).innerHTML;
+            if(context1.indexOf("姓名") != -1) output = name1;
+            if(context1.indexOf("学号") != -1) output = id1;
+            if(context1.indexOf("QQ") != -1 || context1.indexOf("qq") != -1) output = qq1;
+            if(context1.indexOf("学院") != -1) output = xueyuan1;
+            if(context1.indexOf("专业") != -1) output = zhuanye1;
+            if(context1.indexOf("手机") != -1 || context1.indexOf("电话") != -1 ||context1.indexOf("号码") != -1) output = phonenum1;
+            if(eval(text[1]) == 1)
+                output = "";
+        }
+        objs[i].focus();
         objs[i].value = output;
         objs[i].blur();
     }
@@ -86,7 +90,10 @@ javascript: void ((function ()
 
 
 })());
-
+/*
 function validate() {
     output = true;
 }
+*/
+//var btn = document.getElementById("submit_button"); 
+//window.setTimeout(btn.click(),2000); 
